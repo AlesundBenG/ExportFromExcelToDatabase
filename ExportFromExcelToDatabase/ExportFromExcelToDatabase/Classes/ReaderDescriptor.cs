243a6fd71203b981b0a2d551bfd71d3faa7e7e00
Паша <77670636+AlesundBenG@users.noreply.github.com>
2021-09-21 18:43:25 +0300

    Чтение дескриптора сделано, но только без вложенных тегов.

--- ExportFromExcelToDatabase/ExportFromExcelToDatabase/Classes/ReaderDescriptor.cs
+++ ExportFromExcelToDatabase/ExportFromExcelToDatabase/Classes/ReaderDescriptor.cs
@@ -18,9 +18,52 @@ namespace ExportFromExcelToDatabase.Classes
             return listDescriptor;
         }
 
-        /// <summary>
-        /// Pattert part: <tag>...</tag>
-        /// </summary>
+        /// <param name="objectsPart">Pattert part: <tag>...</tag></param>
+        private DescriptorObject getDescriptorObject(string objectsPart) {
+            DescriptorObject descriptor = new DescriptorObject();
+            descriptor.NameObject = objectsPart.Substring(1, objectsPart.IndexOf('>') - 1);
+            List<string> tokens = splitObjectsPartOnToken(objectsPart.Replace($"<{descriptor.NameObject}>", "").Replace($"</{descriptor.NameObject}>", ""));
+            for (int i = 0; i < tokens.Count; i++) {
+                descriptor.addToken(getToken(tokens[i]));
+            }
+            return descriptor;
+        }
+
+        private Token getToken(string pathToken) {
+            int startPosition = getPositionAfterSpace(pathToken, 0);
+            int endPosition = getPositionNextSpace(pathToken, startPosition) - 1;
+            string name = pathToken.Substring(startPosition, endPosition - startPosition - 1);
+            string value = getStringBetweenQuotation(pathToken, endPosition);
+            return new Token() {
+                Name = name,
+                Value = value
+            };
+        }
+
+        private List<string> splitObjectsPartOnToken(string objectsPath) {
+            List<string> listPart = new List<string>();
+            string part = "";
+            int startPosition = 0;
+            int currentPosition = 0;
+            bool betweenQuotation = false;
+            while (currentPosition < objectsPath.Length) {
+                if (objectsPath[currentPosition] == '\"') {
+                    betweenQuotation = !betweenQuotation;
+                }
+                else if ((objectsPath[currentPosition] == ';') && (!betweenQuotation)) {
+                    part = objectsPath.Substring(startPosition, currentPosition - startPosition + 1);
+                    listPart.Add(part);
+                    startPosition = currentPosition + 1;
+                }
+                currentPosition++;
+            }
+            part = objectsPath.Substring(startPosition, currentPosition - startPosition);
+            if (part.Replace('\t', ' ').Replace(" ", "").Length > 0) {
+                listPart.Add(part);
+            }
+            return listPart;
+        }
+
         private List<string> splitDescriptorOnObjectsPart(string descriptor) {
             List<string> listPart = new List<string>();
             string tag;
@@ -57,13 +100,6 @@ namespace ExportFromExcelToDatabase.Classes
         }
 
 
-        /// <param name="objectsPart">Pattert part: <tag>...</tag></param>
-        private DescriptorObject getDescriptorObject(string objectsPart) {
-            DescriptorObject descriptor = new DescriptorObject();
-            descriptor.NameObject = objectsPart.Substring(1, objectsPart.IndexOf('>') - 1);
-            return descriptor;
-        }
-
         private int getPositionAfterSpace(string text, int startPosition) {
             int currentPosition = startPosition;
             while (currentPosition < text.Length) {
@@ -75,6 +111,43 @@ namespace ExportFromExcelToDatabase.Classes
                 }
             }
             return -1;
+        }
+
+        private int getPositionNextSpace(string text, int startPosition) {
+            int currentPosition = startPosition;
+            while (currentPosition < text.Length) {
+                if ((text[currentPosition] == ' ') || (text[currentPosition] == '\t')) {
+                    return currentPosition;
+                }
+                else {
+                    currentPosition++;
+                }
+            }
+            return -1;
+        }
+
+        private string getStringBetweenQuotation(string text, int startPosition) {
+            int currentPosition = startPosition;
+            int firstQuotationPosition = currentPosition;
+            bool foundFirst = false;
+            while (currentPosition < text.Length) {
+                if (text[currentPosition] == '\"')  {
+                    if ((text[currentPosition - 1] != '\\') && (!foundFirst)) {
+                        foundFirst = true;
+                        firstQuotationPosition = currentPosition;
+                    }
+                    else if ((text[currentPosition - 1] != '\\') && (foundFirst)) {
+                        return text.Substring(firstQuotationPosition + 1, currentPosition - firstQuotationPosition - 1);
+                    }
+                }
+                currentPosition++;
+            }
+            if (foundFirst) {
+                throw new Exception($"Not found second quotation: \"{text.Substring(firstQuotationPosition, text.Length - firstQuotationPosition)}");
+            }
+            else {
+                throw new Exception($"Not found first quotation: \"{text.Substring(startPosition, text.Length - startPosition)}");
+            }
         }
     }
 }
